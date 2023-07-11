6db939fd943a8ccc3743ada04aa3db2337f16c03
HidetaroTanaka <s227891v@st.me.tuat.ac.jp>
2023-07-11 19:43:14 +0900

    build script outputs each hex for data and inst

--- src/main/resources/build.sh
+++ src/main/resources/build.sh
@@ -4,4 +4,8 @@
 riscv64-unknown-elf-gcc -march=rv64i -mabi=lp64 -static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles -I ./headers/ -T link.ld $1 -o test.out
 riscv64-unknown-elf-objdump --disassemble-all --disassemble-zeroes --section=.text --section=.text.startup --section=.text.init --section=.data test.out > test.dump
 riscv64-unknown-elf-objcopy -O binary test.out test.bin
-hexdump -v -e '1/1 "%02x" "\n"' test.bin > testdump.hex
+riscv64-unknown-elf-objcopy --dump-section .data=test_data.bin test.out
+riscv64-unknown-elf-objcopy --dump-section .text.init=test_inst.bin test.out
+hexdump -v -e '1/1 "%02x" "\n"' test_data.bin > testdump_data.hex
+hexdump -v -e '1/1 "%02x" "\n"' test_inst.bin > testdump_inst.hex
+# hexdump -v -e '1/1 "%02x" "\n"' test.bin > testdump.hex
