0295e38a8fd44b5e3fde1ee1cd0aa771cdd8258d
Hidetaro Tanaka <s227891v@st.me.tuat.ac.jp>
2023-08-01 00:25:14 +0900

    aaaaaa

--- src/main/scala/hajime/publicmodules/LDSTUnit.scala
+++ src/main/scala/hajime/publicmodules/LDSTUnit.scala
@@ -1,7 +1,7 @@
 package hajime.publicmodules
 
+import circt.stage.ChiselStage
 import chisel3._
-import chisel3.stage.ChiselStage
 import chisel3.util._
 import hajime.axiIO.AXI4liteIO
 import hajime.common._
@@ -42,26 +42,27 @@ class LDSTUnit(implicit params: HajimeCoreParams) extends Module with ScalarOpCo
     (io.cpu.req.bits.funct.memory_function === MEM_FCN.M_WR.asUInt) -> (io.dcache_axi4lite.aw.ready && io.dcache_axi4lite.w.ready),
   ))
   io.cpu.resp.valid := MuxCase(true.B, Seq(
-    req_reg.MEM_ctrl.memRead -> io.dcache_axi4lite.r.valid,
-    req_reg.MEM_ctrl.memWrite -> io.dcache_axi4lite.b.valid,
+    (req_reg.funct.memory_function === MEM_FCN.M_RD.asUInt) -> io.dcache_axi4lite.r.valid,
+    (req_reg.funct.memory_function === MEM_FCN.M_WR.asUInt) -> io.dcache_axi4lite.b.valid,
   ))
   io.cpu.resp.bits.exception := MuxCase(false.B, Seq(
-    req_reg.MEM_ctrl.memRead -> io.dcache_axi4lite.r.bits.exception,
-    req_reg.MEM_ctrl.memWrite -> io.dcache_axi4lite.b.bits.exception,
+    (req_reg.funct.memory_function === MEM_FCN.M_RD.asUInt) -> io.dcache_axi4lite.r.bits.exception,
+    (req_reg.funct.memory_function === MEM_FCN.M_WR.asUInt) -> io.dcache_axi4lite.b.bits.exception,
   ))
-  io.cpu.resp.bits.data := MuxLookup(req_reg.MEM_ctrl.mem_func, io.dcache_axi4lite.r.bits.data)(Seq(
-    CACHE_FUNCTIONS.BYTE -> Mux(req_reg.MEM_ctrl.mem_sext, hajime.common.Functions.sign_ext(io.dcache_axi4lite.r.bits.data(7,0), xprlen), io.dcache_axi4lite.r.bits.data(7,0).zext.asUInt),
-    CACHE_FUNCTIONS.HALFWORD -> Mux(req_reg.MEM_ctrl.mem_sext, hajime.common.Functions.sign_ext(io.dcache_axi4lite.r.bits.data(15,0), xprlen), io.dcache_axi4lite.r.bits.data(15,0).zext.asUInt),
-    CACHE_FUNCTIONS.WORD -> Mux(req_reg.MEM_ctrl.mem_sext, hajime.common.Functions.sign_ext(io.dcache_axi4lite.r.bits.data(31,0), xprlen), io.dcache_axi4lite.r.bits.data(31,0).zext.asUInt),
+  io.cpu.resp.bits.data := MuxLookup(req_reg.funct.memory_length, io.dcache_axi4lite.r.bits.data)(Seq(
+    MEM_LEN.B.asUInt -> Mux(req_reg.funct.mem_sext.asBool, hajime.common.Functions.sign_ext(io.dcache_axi4lite.r.bits.data(7,0), params.xprlen), io.dcache_axi4lite.r.bits.data(7,0).zext.asUInt),
+    MEM_LEN.H.asUInt -> Mux(req_reg.funct.mem_sext.asBool, hajime.common.Functions.sign_ext(io.dcache_axi4lite.r.bits.data(15,0), params.xprlen), io.dcache_axi4lite.r.bits.data(15,0).zext.asUInt),
+    MEM_LEN.W.asUInt -> Mux(req_reg.funct.mem_sext.asBool, hajime.common.Functions.sign_ext(io.dcache_axi4lite.r.bits.data(31,0), params.xprlen), io.dcache_axi4lite.r.bits.data(31,0).zext.asUInt),
+    MEM_LEN.D.asUInt -> io.dcache_axi4lite.r.bits.data
   ))
 
   // AXI4Lite Read Address Request Channel
-  io.dcache_axi4lite.ar.valid := io.cpu.req.bits.MEM_ctrl.memRead && io.cpu.req.valid
+  io.dcache_axi4lite.ar.valid := (io.cpu.req.bits.funct.memory_function === MEM_FCN.M_RD.asUInt) && io.cpu.req.valid
   io.dcache_axi4lite.ar.bits.addr := io.cpu.req.bits.addr
   io.dcache_axi4lite.ar.bits.prot := 0.U(3.W)
 
   // AXI4Lite Write Address Request Channel
-  io.dcache_axi4lite.aw.valid := io.cpu.req.bits.MEM_ctrl.memWrite && io.cpu.req.valid
+  io.dcache_axi4lite.aw.valid := (io.cpu.req.bits.funct.memory_function === MEM_FCN.M_WR.asUInt) && io.cpu.req.valid
   io.dcache_axi4lite.aw.bits.addr := io.cpu.req.bits.addr
   io.dcache_axi4lite.aw.bits.prot := 0.U(3.W)
 
@@ -72,19 +73,18 @@ class LDSTUnit(implicit params: HajimeCoreParams) extends Module with ScalarOpCo
   io.dcache_axi4lite.r.ready := true.B
 
   // AXI4Lite Write Data Request Channel
-  io.dcache_axi4lite.w.valid := io.cpu.req.bits.MEM_ctrl.memWrite && io.cpu.req.valid
+  io.dcache_axi4lite.w.valid := (io.cpu.req.bits.funct.memory_function === MEM_FCN.M_WR.asUInt) && io.cpu.req.valid
   io.dcache_axi4lite.w.bits.data := io.cpu.req.bits.data
-  io.dcache_axi4lite.w.bits.strb := MuxLookup(io.cpu.req.bits.MEM_ctrl.mem_func, 0.U(strb_width.W))(Seq(
-    CACHE_FUNCTIONS.BYTE -> "h01".U(strb_width.W),
-    CACHE_FUNCTIONS.HALFWORD -> "h03".U(strb_width.W),
-    CACHE_FUNCTIONS.WORD -> "h0F".U(strb_width.W),
-    CACHE_FUNCTIONS.DOUBLEWORD -> "hFF".U(strb_width.W)
+  io.dcache_axi4lite.w.bits.strb := MuxLookup(io.cpu.req.bits.funct.memory_length, 0.U(strb_width.W))(Seq(
+    MEM_LEN.B.asUInt -> "h01".U(strb_width.W),
+    MEM_LEN.H.asUInt -> "h03".U(strb_width.W),
+    MEM_LEN.W.asUInt-> "h0F".U(strb_width.W),
+    MEM_LEN.D.asUInt -> "hFF".U(strb_width.W)
   ))
 }
 
 object LDSTUnit extends App {
-  def apply(xprlen: Int): LDSTUnit = new LDSTUnit(xprlen)
+  def apply(implicit params: HajimeCoreParams): LDSTUnit = new LDSTUnit()
 
-  (new ChiselStage).emitVerilog(this.apply(xprlen = RISCV_Consts.XLEN), args = Array(
-    "--emission-options=disableMemRandomization,disableRegisterRandomization"))
+  (ChiselStage).emitSystemVerilogFile(LDSTUnit(HajimeCoreParams()), firtoolOpts = COMPILE_CONSTANTS.FIRTOOLOPS)
 }
