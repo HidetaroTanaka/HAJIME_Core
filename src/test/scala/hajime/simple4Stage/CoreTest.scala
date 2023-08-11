9a008cc9a698719ada0fe4e6f57f618aa66beab6
HidetaroTanaka <s227891v@st.me.tuat.ac.jp>
2023-08-11 15:54:25 +0900

    dcache resp is for addr(63,3), so CPU must shift data

--- src/test/scala/hajime/simple4Stage/CoreTest.scala
+++ src/test/scala/hajime/simple4Stage/CoreTest.scala
@@ -28,7 +28,7 @@ class CoreTest extends AnyFlatSpec with ChiselScalatestTester {
       // 0x0000_0000 ~ 0x0000_1FFF : icache
       // 0x0000_4000 ~ 0x0000_5FFF : dcache
       // 0x1000_0000               : tohost
-      test(new Core_and_cache(dcache_hexfilename = null, icache_memsize = 8192, dcache_memsize = 8192, tohost = 0x10000000)).withAnnotations(Seq(WriteVcdAnnotation, VerilatorBackendAnnotation)) { dut =>
+      test(new Core_and_cache(icache_memsize = 8192, dcache_memsize = 8192, tohost = 0x10000000)).withAnnotations(Seq(WriteVcdAnnotation, VerilatorBackendAnnotation)) { dut =>
         dut.clock.setTimeout(1024)
         dut.io.reset_vector.poke(0.U)
         dut.io.hartid.poke(0.U)
@@ -70,7 +70,7 @@ class CoreTest extends AnyFlatSpec with ChiselScalatestTester {
     }
   }
   val instList_withDmem = Seq(
-    // "lb", "lbu", "ld", "lh", "lhu", "lw", "lwu", "ma_data",
+    "lb", // "lbu", "ld", "lh", "lhu", "lw", "lwu",
     // "sb", "sd", "sh", "sw"
   )
   // make it true only when recompile riscv-tests
@@ -81,13 +81,77 @@ class CoreTest extends AnyFlatSpec with ChiselScalatestTester {
   }
   for(e <- instList_withDmem) {
     it should s"pass the test ${e}" in {
-      test(new Core_and_cache(dcache_hexfilename = s"src/main/resources/rv64ui/${e}_data.hex", icache_memsize = 8192, dcache_memsize = 8192, tohost = 0x10000000)).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
-        c.clock.setTimeout(if(e == "ma_data") 4096 else 1024)
-        while (c.io.toHost.peek().litValue == 0) {
-          c.clock.step()
+      test(new Core_and_cache(icache_memsize = 8192, dcache_memsize = 8192, tohost = 0x10000000)).withAnnotations(Seq(WriteVcdAnnotation, VerilatorBackendAnnotation)) { dut =>
+        def initialiseImem(filename: String): Unit = {
+          dut.io.imem_initialiseAXI.ar.bits.addr.poke(0.U)
+          dut.io.imem_initialiseAXI.ar.bits.prot.poke(0.U)
+          dut.io.imem_initialiseAXI.ar.valid.poke(false.B)
+          dut.io.imem_initialiseAXI.aw.bits.addr.poke(0.U)
+          dut.io.imem_initialiseAXI.aw.bits.prot.poke(0.U)
+          dut.io.imem_initialiseAXI.aw.valid.poke(false.B)
+          dut.io.imem_initialiseAXI.b.ready.poke(true.B)
+          dut.io.imem_initialiseAXI.r.ready.poke(true.B)
+          dut.io.imem_initialiseAXI.w.bits.data.poke(0.U)
+          dut.io.imem_initialiseAXI.w.bits.strb.poke(0.U)
+          dut.io.imem_initialiseAXI.w.valid.poke(false.B)
+          dut.io.icache_initialising.poke(true.B)
+          // initialise icache from file
+          val fileSource = Source.fromFile(filename)
+          for ((data, idx) <- fileSource.getLines().zipWithIndex) {
+            dut.io.imem_initialiseAXI.aw.bits.addr.poke((idx * 4).U)
+            dut.io.imem_initialiseAXI.aw.valid.poke(true.B)
+            dut.io.imem_initialiseAXI.w.bits.data.poke(s"h$data".U(32.W))
+            dut.io.imem_initialiseAXI.w.bits.strb.poke(0xF.U(8.W))
+            dut.io.imem_initialiseAXI.w.valid.poke(true.B)
+            dut.clock.step()
+          }
+          fileSource.close()
+          dut.io.imem_initialiseAXI.aw.valid.poke(false.B)
+          dut.io.imem_initialiseAXI.w.valid.poke(false.B)
+          dut.io.icache_initialising.poke(false.B)
         }
-        c.io.toHost.bits.expect("h01".U(64.W))
-        val toHost_Value = c.io.toHost.bits.peek().litValue
+        def initialiseDmem(filename: String): Unit = {
+          dut.io.dmem_initialiseAXI.ar.bits.addr.poke(0.U)
+          dut.io.dmem_initialiseAXI.ar.bits.prot.poke(0.U)
+          dut.io.dmem_initialiseAXI.ar.valid.poke(false.B)
+          dut.io.dmem_initialiseAXI.aw.bits.addr.poke(0.U)
+          dut.io.dmem_initialiseAXI.aw.bits.prot.poke(0.U)
+          dut.io.dmem_initialiseAXI.aw.valid.poke(false.B)
+          dut.io.dmem_initialiseAXI.b.ready.poke(true.B)
+          dut.io.dmem_initialiseAXI.r.ready.poke(true.B)
+          dut.io.dmem_initialiseAXI.w.bits.data.poke(0.U)
+          dut.io.dmem_initialiseAXI.w.bits.strb.poke(0.U)
+          dut.io.dmem_initialiseAXI.w.valid.poke(false.B)
+          dut.io.dcache_initialising.poke(true.B)
+          // initialise icache from file
+          val fileSource = Source.fromFile(filename)
+          for ((data, idx) <- fileSource.getLines().zipWithIndex) {
+            dut.io.dmem_initialiseAXI.aw.bits.addr.poke((idx * 4).U)
+            dut.io.dmem_initialiseAXI.aw.valid.poke(true.B)
+            dut.io.dmem_initialiseAXI.w.bits.data.poke(s"h$data".U(32.W))
+            dut.io.dmem_initialiseAXI.w.bits.strb.poke(0xF.U(8.W))
+            dut.io.dmem_initialiseAXI.w.valid.poke(true.B)
+            dut.clock.step()
+          }
+          fileSource.close()
+          dut.io.dmem_initialiseAXI.aw.valid.poke(false.B)
+          dut.io.dmem_initialiseAXI.w.valid.poke(false.B)
+          dut.io.dcache_initialising.poke(false.B)
+        }
+        dut.clock.setTimeout(1024)
+        dut.io.reset_vector.poke(0.U)
+        dut.io.hartid.poke(0.U)
+        fork {
+          initialiseImem(s"src/main/resources/rv64ui/${e}_inst.hex")
+        } .fork {
+          initialiseDmem(s"src/main/resources/rv64ui/${e}_data.hex")
+        } .join()
+
+        while (dut.io.toHost.peek().litValue == 0) {
+          dut.clock.step()
+        }
+        dut.io.toHost.bits.expect("h01".U(64.W))
+        val toHost_Value = dut.io.toHost.bits.peek().litValue
         if (toHost_Value == 1) println(s"${e} test passed.") else println(s"${e} test failed at ${toHost_Value}")
         // println(s"IPC for ${e} test: ${c.io.performance_counters.retired_inst_count.peek().litValue.toDouble / c.io.performance_counters.cycle_count.peek().litValue.toDouble}")
       }
--- src/test/scala/hajime/simple4Stage/Core_and_cache.scala
+++ src/test/scala/hajime/simple4Stage/Core_and_cache.scala
@@ -7,7 +7,7 @@ import hajime.axiIO.AXI4liteIO
 import hajime.common._
 import hajime.publicmodules._
 
-class Core_and_cache(dcache_hexfilename: String, icache_memsize: Int = 8192, dcache_memsize: Int = 8192, tohost: Int = 0x10000000) extends Module {
+class Core_and_cache(icache_memsize: Int = 8192, dcache_memsize: Int = 8192, tohost: Int = 0x10000000) extends Module {
   val params = HajimeCoreParams()
   val io = IO(new Bundle{
     val reset_vector = Input(UInt(64.W))
@@ -15,18 +15,24 @@ class Core_and_cache(dcache_hexfilename: String, icache_memsize: Int = 8192, dca
     val toHost = ValidIO(UInt(64.W))
     val debug_io = Output(debugIO(params))
     val icache_initialising = Input(Bool())
+    val dcache_initialising = Input(Bool())
     val imem_initialiseAXI = Flipped(new AXI4liteIO(addr_width = 64, data_width = 32))
+    val dmem_initialiseAXI = Flipped(new AXI4liteIO(addr_width = 64, data_width = 64))
   })
 
-  val core = withReset(io.icache_initialising || reset.asBool) {
+  val core = withReset(io.icache_initialising || io.dcache_initialising || reset.asBool) {
     Module(Core(params))
   }
   val icache = Module(Icache_for_Verilator(memsize = icache_memsize))
-  val dcache = Module(new Dcache_model(dcacheBaseAddr = 0x00004000, tohost = tohost, hexfileName = dcache_hexfilename, memsize = dcache_memsize))
+  val dcache = Module(Dcache_for_Verilator(dcacheBaseAddr = 0x00004000, tohost = tohost, memsize = dcache_memsize))
 
   icache.io := DontCare
   io.imem_initialiseAXI := DontCare
   core.io.icache_axi4lite := DontCare
+  dcache.io := DontCare
+  io.dmem_initialiseAXI := DontCare
+  core.io.dcache_axi4lite := DontCare
+
   when(io.icache_initialising) {
     icache.io <> io.imem_initialiseAXI
     core.io.icache_axi4lite.ar.ready := false.B
@@ -38,7 +44,18 @@ class Core_and_cache(dcache_hexfilename: String, icache_memsize: Int = 8192, dca
     io.imem_initialiseAXI.aw.ready := false.B
     io.imem_initialiseAXI.w.ready := false.B
   }
-  core.io.dcache_axi4lite <> dcache.io
+
+  when(io.dcache_initialising) {
+    dcache.io <> io.dmem_initialiseAXI
+    core.io.dcache_axi4lite.ar.ready := false.B
+    core.io.dcache_axi4lite.aw.ready := false.B
+    core.io.dcache_axi4lite.w.ready := false.B
+  } .otherwise {
+    dcache.io <> core.io.dcache_axi4lite
+    io.dmem_initialiseAXI.ar.ready := false.B
+    io.dmem_initialiseAXI.aw.ready := false.B
+    io.dmem_initialiseAXI.w.ready := false.B
+  }
 
   core.io.reset_vector := io.reset_vector
   core.io.hartid := io.hartid
@@ -47,6 +64,6 @@ class Core_and_cache(dcache_hexfilename: String, icache_memsize: Int = 8192, dca
 }
 
 object Core_and_cache extends App {
-  def apply(dcache_hexfilename: String, icache_memsize: Int, dcache_memsize: Int, tohost: Int): Core_and_cache = new Core_and_cache(dcache_hexfilename, icache_memsize, dcache_memsize, tohost)
-  ChiselStage.emitSystemVerilogFile(apply(dcache_hexfilename = null, icache_memsize = 8192, dcache_memsize = 8192, tohost = 0x10000000), firtoolOpts = COMPILE_CONSTANTS.FIRTOOLOPS)
+  def apply(icache_memsize: Int, dcache_memsize: Int, tohost: Int): Core_and_cache = new Core_and_cache(icache_memsize, dcache_memsize, tohost)
+  ChiselStage.emitSystemVerilogFile(apply(icache_memsize = 8192, dcache_memsize = 8192, tohost = 0x10000000), firtoolOpts = COMPILE_CONSTANTS.FIRTOOLOPS)
 }
