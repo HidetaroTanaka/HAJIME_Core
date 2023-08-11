e9d36a783c6a1f022fb1427f6b41a5dff7374b16
HidetaroTanaka <s227891v@st.me.tuat.ac.jp>
2023-08-11 15:24:06 +0900

    Dcache_for_Verilator

--- src/test/scala/hajime/publicmodules/Dcache_for_Verilator.scala
+++ src/test/scala/hajime/publicmodules/Dcache_for_Verilator.scala
@@ -3,13 +3,15 @@ import chisel3._
 import circt.stage.ChiselStage
 import chisel3.util._
 import chisel3.experimental.BundleLiterals._
-import hajime.axiIO.AXI4liteIO
+import hajime.axiIO._
 import hajime.common.COMPILE_CONSTANTS
 import chiseltest._
 import org.scalatest.flatspec._
+
 import scala.io._
 
-class Dcache_for_Verilator(dcacheBaseAddr: Int, tohost: Int, memsize: Int = 0x2000) extends Module {
+// 命令キャッシュと異なりマスター側のreadyが下がることは無いので，出力のストールは考えない
+class Dcache_for_Verilator(dcacheBaseAddr: Int, tohost: Int, memsize: Int = 0x2000) extends Module with ChecksAxiReadResp with ChecksAxiWriteResp{
   require(memsize % 8 == 0, s"memsize $memsize is not multiple of 8")
 
   val io = IO(Flipped(new AXI4liteIO(addr_width = 64, data_width = 64)))
@@ -20,6 +22,7 @@ class Dcache_for_Verilator(dcacheBaseAddr: Int, tohost: Int, memsize: Int = 0x20
     _.valid -> false.B,
   )))
   debug := debugToHost
+  debugToHost.valid := false.B
 
   io.aw.ready := true.B
   io.ar.ready := true.B
@@ -29,15 +32,37 @@ class Dcache_for_Verilator(dcacheBaseAddr: Int, tohost: Int, memsize: Int = 0x20
   val internalWriteAddr = io.aw.bits.addr - dcacheBaseAddr.U
 
   // 0x000 ~ 0x1FFC
-  // D$ Address space from Core: 0x00004000 ~ 0x00005FFFC for word
-  // doubleWord at 0x0000: 0x0000 ~ 0x0007
-  val mem = SyncReadMem(memsize/8, Vec(8, UInt(8.W)))
+  val mem = SyncReadMem(memsize/2, Vec(8, UInt(8.W)))
 
+  // read
   val readDataFromMem = Wire(chiselTypeOf(io.r.bits))
-  readDataFromMem.data := Cat(mem.read(io.ar.bits.addr.head(61)).reverse)
+  readDataFromMem.data := Cat(mem.read(internalReadAddr.head(61)).reverse)
   // Byte: 0x1FFF以下
   // HalfWord: 0x1FFE以下，addr+1が8Byteを超えない
   // Word: 0x1FFC以下，addr+3が8Byteを超えない
   // DoubleWord: 0x1FF8以下，整列のみ
-  // readDataFromMem.resp := Mux()
+  // メモリロードの際にわからないので，全てコアに丸投げすることとする
+  readDataFromMem.resp := R_OKEY.U
+
+  io.r.bits := readDataFromMem
+  io.r.valid := RegNext(io.ar.valid && io.ar.ready)
+
+  // write
+  val writeData_asVec = Wire(Vec(8, UInt(8.W)))
+  for((w,i) <- writeData_asVec.zipWithIndex) {
+    w := io.w.bits.data(8*i+7, 8*i)
+  }
+  when(io.aw.valid && io.w.valid && internalWriteAddr < 0x00001FFF.U) {
+    mem.write(internalWriteAddr.head(62), writeData_asVec, io.w.bits.strb.asBools)
+  } .elsewhen(io.aw.valid && io.w.valid && io.aw.bits.addr === tohost.U) {
+    debugToHost.bits := io.w.bits.data
+    debugToHost.valid := true.B
+  }
+  io.b.valid := RegNext(io.aw.valid && io.w.valid)
+  io.b.bits.resp := W_OKEY.U
+}
+
+object Dcache_for_Verilator extends App {
+  def apply(dcacheBaseAddr: Int = 0x00004000, tohost: Int = 0x10000000, memsize: Int = 0x2000): Dcache_for_Verilator = new Dcache_for_Verilator(dcacheBaseAddr, tohost, memsize)
+  ChiselStage.emitSystemVerilogFile(Dcache_for_Verilator(), firtoolOpts = COMPILE_CONSTANTS.FIRTOOLOPS)
 }
